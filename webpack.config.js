049a5de7e4e1b7e6eaca3b59594995bf7406085d
Robert Zobrist <robertzobrist@Roberts-iMac.local>
2017-12-04 11:24:57 -0700

    fixed an error in the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -24,8 +24,6 @@ module.exports = {
             }
           }
         ]
-      },resolve: {
-        extensions: [‘’, ‘.js’, ‘.jsx’]
       },
       // This lets us debug our react code in chrome dev tools. Errors will have lines and file names
       // Without this the console says all errors are coming from just coming from bundle.js

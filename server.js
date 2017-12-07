a3ae965ec9615c0d0bf5f9e6c3f6a19bfdd9abf5
Robert Zobrist <robertzobrist@Roberts-iMac.local>
2017-12-07 14:41:24 -0700

    added sequeuelize again

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.get("*", function(req, res) {
 // -------------------------------------------------
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force:true}).then(function() {
   app.listen(PORT, function(req, res) {
       
     console.log("App listening on PORT " + PORT);

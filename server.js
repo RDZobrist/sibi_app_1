eb79705d2459a87313dbc6697131fd572e141c59
Robert Zobrist <robertzobrist@Roberts-iMac.local>
2017-12-07 19:09:30 -0700

    trying to figure id nor null error

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const bodyParser = require("body-parser");
 const logger = require("morgan");
 const promise = require("bluebird");
 const uuidv1 = require('uuid/v1');
-var current_id;
-var last_id;
+
 // Require History Schema
 const db = require("./server/models");
 
@@ -35,6 +34,8 @@ app.get('/users', function(req,res){
 })
 // -------------------------------------------------
 app.post("/api/saved", function(req, res) {
+  var current_id;
+  var last_id;
   // create takes an argument of an object describing the item we want to
 let localIDuuid = uuidv1();
 
@@ -47,37 +48,41 @@ db.sibi_americans.findAll({
   order: [ [ 'id', 'DESC' ]]
 }).then(function(entries){
   last_id = entries[0].id;
-  current_id = last_id + 1;
-}); 
-if(last_id > 3000 && current_id >= 3001){
-const sibi_americans = db.sibi_americans.build({
-  id:current_id,
-  Title: req.body.Title,
-  GivenName: req.body.GivenName,
-  MiddleInitial: req.body.MiddleInitial,
-  Surname:req.body.Surname,
-  StreetAddress: req.body.StreetAddress,
-  State: req.body.State,
-  ZipCode: req.body.ZipCode,
-  City: req.body.City,
-  EmailAddress: req.body.EmailAddress,
-  Username: req.body.Username,
-  Password: req.body.Password,
-  BrowserUserAgent: req.body.BrowserUserAgent,
-  TelephoneNumber: req.body.TelephoneNumber,
-  Birthday: req.body.Birthday,
-  Color: req.body.Color,
-  Ocupation: req.body.Ocupation,
-  Company: req.body.Company,
-  Vehicle: req.body.Vehicle,
-  Domain: req.body.Domain,   
-  GUID: localIDuuid
-}).save().then(newUser => {
-  console.log(newUser)
-}).catch(error => {
-  console.log(error)
+  current_id = last_id  + 1;
+  // if last_id and current_id
+  // save newUser to db
+  if(last_id > 3000 && current_id >= 3001){
+    console.log('yay')
+    const sibi_americans = db.sibi_americans.build({
+      id:current_id,
+      Title: req.body.Title,
+      GivenName: req.body.GivenName,
+      MiddleInitial: req.body.MiddleInitial,
+      Surname:req.body.Surname,
+      StreetAddress: req.body.StreetAddress,
+      State: req.body.State,
+      ZipCode: req.body.ZipCode,
+      City: req.body.City,
+      EmailAddress: req.body.EmailAddress,
+      Username: req.body.Username,
+      Password: req.body.Password,
+      BrowserUserAgent: req.body.BrowserUserAgent,
+      TelephoneNumber: req.body.TelephoneNumber,
+      Birthday: req.body.Birthday,
+      Color: req.body.Color,
+      Ocupation: req.body.Ocupation,
+      Company: req.body.Company,
+      Vehicle: req.body.Vehicle,
+      Domain: req.body.Domain,   
+      GUID: localIDuuid
+    }).save().then(newUser => {
+      console.log(newUser)
+    }).catch(error => {
+      console.log(error)
+    })
+    }
 })
-}
+
 });
  
 
